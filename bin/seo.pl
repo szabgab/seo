6b8d716a660afb93e95dab777f8962d1c1cb552f
Gabor Szabo <gabor@szabgab.com>
2014-10-06 18:31:47 +0300

    more seo

--- bin/seo.pl
+++ bin/seo.pl
@@ -23,17 +23,39 @@ exit;
 sub main {
 	my ($url) = @_;
 	my $html;
-	
+
 	if ($url =~ m{^https?://}) {
 		$html = get $url;
 	} else {
 		$html = path($url)->slurp_utf8;
 	}
-	
+
 	my $tree= HTML::TreeBuilder::XPath->new;
 	$tree->parse($html);
-	
+
+	_check_title($tree);
+	_check_description($tree);
+
+}
+
+sub _check_description {
+	my ($tree) = @_;
+	say 'Checking descripton';
+	my $elem = $tree->findnodes( '/html/head/meta[@name="description"]')->[0];
+	if (not $elem) {
+		_warn('<meta name="description" content=""> is missing from <head>');
+	} else {
+		say $elem->attr('content');
+	}
+}
+
+sub _check_title {
+	my ($tree) = @_;
 	say 'Checking title';
+	# TODO: reddit actually only looks at the first 1000 characters when looking for a title
+	# and then it tries in the first 10240 characters
+	# https://github.com/reddit/reddit/blob/master/r2/r2/lib/utils/utils.py
+	# see also http://ogp.me/  specificall the og:title attribute
 	my $title = $tree->findvalue( '/html/head/title');
 	my $TITLE_MAX = 100;
 	my $TITLE_MIN = 30;
@@ -45,7 +67,7 @@ sub main {
 		_warn("<title> might be too short to be effective (should be > $TITLE_MIN)");
 	}
 	# TODO textual analyzis, eg. word count ?
-	#say $title;
+	say $title;
 }
 
 sub _warn {
